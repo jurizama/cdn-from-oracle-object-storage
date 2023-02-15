4f4d4865d5f7ec96d3ea9a1beea1fe0bdc692491
Pajri Zahrawaani Ahmad <pajriblueholigand@gmail.com>
2023-02-15 16:28:17 +0700

    Update api.py

--- api.py
+++ api.py
@@ -40,6 +40,6 @@ class _s3(Resource):
 
 api.add_resource(_s3,"/")
 if __name__ == "__main__":
-	app.run(debug=False,port=os.getenv("PORT")
+	app.run(debug=False,port=os.getenv("PORT"))
 
 #cdn access = https://cdn.comnetbe.my.id/
